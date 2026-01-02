af9a3e325c52e225602c9c4979d4ed9d3fd7fd32
mimvoid <mimvoid@proton.me>
2026-01-02 16:00:39 -0500

    Add primitive drawing methods to Renderer

--- include/Renderer.hpp
+++ include/Renderer.hpp
@@ -28,6 +28,96 @@ struct Renderer {
   bool setDrawColor(Rgb rgb);
   bool setDrawColor(Rgba rgba);
 
+  /**
+   * Draw a line with a set width.
+   *
+   * @param x1 x-coordinate of the start point.
+   * @param y1 y-coordinate of the start point.
+   * @param x2 x-coordinate of the end point.
+   * @param y2 y-coordinate of the end point.
+   * @param lineWidth Line width.
+   *
+   * @return `true` on success, `false` on failure.
+   */
+  bool drawLine(float x1, float y1, float x2, float y2, float lineWidth = 1.0f);
+
+  /**
+   * Draw an unfilled rectangle with a set line width.
+   *
+   * @param x x-coordinate of the top left corner.
+   * @param y y-coordinate of the top left corner.
+   * @param w Width of the rectangle.
+   * @param h Height of the rectangle.
+   * @param lineWidth Line width.
+   *
+   * @return `true` on success, `false` on failure.
+   */
+  bool drawRect(float x, float y, float w, float h, float lineWidth = 1.0f);
+
+  /**
+   * Draw a filled rectangle with a set line width.
+   *
+   * @param x x-coordinate of the top left corner.
+   * @param y y-coordinate of the top left corner.
+   * @param w Width of the rectangle.
+   * @param h Height of the rectangle.
+   * @param lineWidth Line width.
+   *
+   * @return `true` on success, `false` on failure.
+   */
+  bool drawRectFill(float x, float y, float w, float h, float lineWidth = 1.0f);
+
+  /**
+   * Draw an aliased (without anti-aliasing) unfilled ellipse.
+   *
+   * @param x x-coordinate of the center.
+   * @param y y-coordinate of the center.
+   * @param rx Horizontal radius.
+   * @param ry Vertical radius.
+   *
+   * @return `true` on success, `false` on failure.
+   */
+  bool aDrawEllipse(float x, float y, float rx, float ry);
+
+  /**
+   * Draw a filled ellipse.
+   *
+   * @param x x-coordinate of the center.
+   * @param y y-coordinate of the center.
+   * @param rx Horizontal radius.
+   * @param ry Vertical radius.
+   *
+   * @return `true` on success, `false` on failure.
+   */
+  bool aDrawEllipseFill(float x, float y, float rx, float ry);
+
+  /**
+   * Draw an aliased (without anti-aliasing) unfilled circle.
+   *
+   * @param rend Renderer to draw on.
+   * @param x x-coordinate of the center.
+   * @param y y-coordinate of the center.
+   * @param r Radius of the circle.
+   *
+   * @return `true` on success, `false` on failure.
+   */
+  constexpr bool aDrawCircle(float x, float y, float r) {
+    return aDrawEllipse(x, y, r, r);
+  }
+
+  /**
+   * Draw an aliased (without anti-aliasing) filled circle.
+   *
+   * @param x x-coordinate of the center.
+   * @param y y-coordinate of the center.
+   * @param r Radius of the circle.
+   *
+   * @return `true` on success, `false` on failure.
+   */
+  constexpr bool aDrawCircleFill(float x, float y, float r) {
+    return aDrawEllipseFill(x, y, r, r);
+  }
+
 private:
   SDL_Renderer *m_data{nullptr};
   TTF_TextEngine *m_textEngine{nullptr};
--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -3,6 +3,7 @@
 #include <SDL3/SDL_video.h>
 #include <SDL3_ttf/SDL_ttf.h>
 #include "Window.hpp"
+#include "draw.hpp"
 
 namespace katze {
 Renderer::Renderer(Window &window)
@@ -35,5 +36,29 @@ bool Renderer::setDrawColor(Rgb rgb) {
 
 bool Renderer::setDrawColor(Rgba rgba) {
   return SDL_SetRenderDrawColor(m_data, rgba.r, rgba.g, rgba.b, rgba.a);
+}
+
+bool Renderer::drawLine(
+  float x1, float y1, float x2, float y2, float lineWidth
+) {
+  return katze::drawLine(m_data, x1, y1, x2, y2, lineWidth);
+}
+
+bool Renderer::drawRect(float x, float y, float w, float h, float lineWidth) {
+  return katze::drawRect(m_data, x, y, w, h, lineWidth);
+}
+
+bool Renderer::drawRectFill(
+  float x, float y, float w, float h, float lineWidth
+) {
+  return katze::drawRectFill(m_data, x, y, w, h, lineWidth);
+}
+
+bool Renderer::aDrawEllipse(float x, float y, float rx, float ry) {
+  return katze::aDrawEllipse(m_data, x, y, rx, ry);
+}
+
+bool Renderer::aDrawEllipseFill(float x, float y, float rx, float ry) {
+  return katze::aDrawEllipseFill(m_data, x, y, rx, ry);
 }
 } // namespace katze
